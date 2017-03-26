6f7de3106c39463fc549abc8aa983b1b9e11c608
Luke M <luke@vibrato.com.au>
2017-03-26 14:11:39 +1100

    Add status label to form

--- js/behaviours.js
+++ js/behaviours.js
@@ -8,6 +8,8 @@ var loadSecrets = function () {
   var plainText = monkey.decrypt(password, cipher);
 
   textArea.val(plainText);
+
+  // Update status here.
 };
 
 var saveSecrets = function () {
@@ -17,6 +19,8 @@ var saveSecrets = function () {
   var cipher = monkey.encrypt(password, plainText);
   // note: Saving the password as the key is ridiculous
   monkey.save(password, cipher);
+
+  // Update status here.
 };
 
 window.behaviours = { loadSecrets: loadSecrets, saveSecrets: saveSecrets };
