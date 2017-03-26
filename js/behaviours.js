01d971423e9370c3dce5f5bd833f70b15dfbd484
Andrew Berry <andrew.berry@outlook.com>
2017-03-26 21:55:24 +1100

    Show message when email field is empty
    #3

--- js/behaviours.js
+++ js/behaviours.js
@@ -24,11 +24,17 @@ var saveSecrets = function () {
   var plainText = jQuery('#secrets').val();
   var password = jQuery('#email').val();
 
+  if (password.length <= 0) {
+    $('#status').text('Invalid Email Address');
+    return;
+  }
+
   var cipher = monkey.encrypt(password, plainText);
   // note: Saving the password as the key is ridiculous
   monkey.save(password, cipher);
 
   // Update status here.
+  $('#status').text('Saved Successfully');
 };
 
 window.behaviours = { loadSecrets: loadSecrets, saveSecrets: saveSecrets };
