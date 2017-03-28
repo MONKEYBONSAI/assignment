7d19a51591c9b9bea886532b046c2ef87f8a5b59
Luke M <luke@vibrato.com.au>
2017-03-28 21:39:04 +1100

    Add JSON loading and saving

--- js/monkey.js
+++ js/monkey.js
@@ -4,8 +4,16 @@ var save = function (key, data) {
   localStorage.setItem(key, data);
 };
 
+var saveJSON = function (key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
 var load = function (key) {
   return localStorage.getItem(key);
+};
+
+var loadJSON = function (key) {
+  return JSON.parse(localStorage.getItem(key));
 };
 
 var encrypt = function (password, plaintext) {
@@ -16,10 +24,14 @@ var decrypt = function (password, encrypted) {
   return sjcl.decrypt(password, encrypted);
 };
 
-var hash = function(value) {
+var hash = function (value) {
   var b = sjcl.hash.sha256.hash(value);  
   return sjcl.codec.hex.fromBits(b);
 };
 
 // hack export via global monkey
-window.monkey = { save: save, load: load, encrypt: encrypt, decrypt: decrypt, hash: hash };
+window.monkey = { 
+  save: save, saveJSON: saveJSON, 
+  load: load, loadJSON: loadJSON,
+  encrypt: encrypt, decrypt: decrypt, 
+  hash: hash };
